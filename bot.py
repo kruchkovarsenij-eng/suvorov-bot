6305b9723316f0f6aa191e03faf72f428d48d0b1
kruchkovarsenij-eng <kruchkovarsenij@gmail.com>
2026-05-31 22:30:54 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,15 +3,14 @@ import os
 import logging
 import asyncio
 from datetime import datetime
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, Bot
 from telegram.ext import Application, CommandHandler, MessageHandler, CallbackQueryHandler, ContextTypes, filters
-from telegram.request import HTTPXRequest
 
-# Отключаем буферизацию вывода логов для Railway
+# Моментальный вывод логов для Railway
 sys.stdout.reconfigure(line_buffering=True)
 sys.stderr.reconfigure(line_buffering=True)
 
-print("--- [DOCKER START] ИНИЦИАЛИЗАЦИЯ СКРИПТА ---", flush=True)
+print("--- [DOCKER START] СТАРТ ЧИСТОГО ЯДРА БОТА ---", flush=True)
 
 TOKEN = "8827819420:AAGS-aXjMvsewGkxAJbBwt2SggWU8Opk5qc"
 ADMIN_CHAT_ID = 8743677274
@@ -136,3 +135,4 @@ async def send_question(chat_id, context, session):
     await context.bot.send_message(chat_id, text, reply_markup=rm, parse_mode="Markdown")
     session["lock"] = False
 
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
